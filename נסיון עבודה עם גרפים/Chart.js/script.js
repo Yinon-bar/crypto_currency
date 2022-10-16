c595c8acbfefc127682a40b05ddaed41f063551a
Yinon Bar <yinonbar1988@gmail.com>
2022-10-16 18:20:55 +0300

    fully working app with charts

--- נסיון עבודה עם גרפים/Chart.js/script.js
+++ נסיון עבודה עם גרפים/Chart.js/script.js
@@ -1,47 +1,55 @@
-const labels = ["January", "February", "March", "April", "May", "June"];
+const labels = [
+  "ירושלים",
+  "תל - אביב",
+  "חיפה",
+  "ראשון לציון",
+  "פתח - תקוה",
+  "נתניה",
+];
 
 const data = {
   labels: labels,
   datasets: [
     {
-      label: "My First dataset",
-      backgroundColor: "rgb(255, 99, 132)",
+      label: "אוכלסיה",
+      backgroundColor: "rgb(255, 186, 132)",
       borderColor: "rgb(255, 99, 132)",
-      data: [0, 10, 5, 2, 20, 30, 45],
+      data: [],
+      borderWidth: 2,
     },
+    // {
+    //   label: "אוכלסיה",
+    //   backgroundColor: "rgb(255, 186, 132)",
+    //   borderColor: "rgb(255, 185, 132)",
+    //   data: [255, 45855, 4584, 45448, 45648, 458845],
+    //   borderWidth: 2,
+    // },
   ],
 };
 
+console.log(data.datasets[0].data);
+
+for (let i = 0; i < labels.length; i++) {
+  let rndNum = Math.floor(Math.random() * 600000) + 50000;
+  data.datasets[0].data.push(rndNum);
+}
+
 const config = {
-  type: "line",
+  type: "bar",
   data: data,
   options: {
-    responsive: true,
-    interaction: {
-      mode: "index",
-      intersect: false,
-    },
-    stacked: false,
     plugins: {
       title: {
         display: true,
-        text: "Chart.js Line Chart - Multi Axis",
+        text: "כמות תושבים לפי עיר",
+        font: {
+          size: 27,
+        },
       },
-    },
-    scales: {
-      y: {
-        type: "linear",
-        display: true,
-        position: "left",
-      },
-      y1: {
-        type: "linear",
-        display: true,
-        position: "right",
-
-        // grid line settings
-        grid: {
-          drawOnChartArea: false, // only want the grid lines for one axis to show up
+      legend: {
+        position: "top",
+        labels: {
+          color: "#444",
         },
       },
     },
@@ -49,3 +57,8 @@ const config = {
 };
 
 const myChart = new Chart(document.getElementById("myChart"), config);
+
+// הגדרת משתנים גלובלים
+Chart.defaults.font.family = "assistant";
+// Chart.defaults.font.size = "15px";
+Chart.defaults.font.weight = "600";
